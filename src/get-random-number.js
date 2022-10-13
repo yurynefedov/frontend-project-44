539bb6dda9ce7a4eb9f67cf40f2753e61d2caf8a
yurynefedov <yury.a.nefedov@gmail.com>
2022-10-13 17:04:25 +0400

    refactor: refactoring get-random-number.js

--- src/get-random-number.js
+++ src/get-random-number.js
@@ -1,1 +1,8 @@
-export default (int = 100) => Math.floor(Math.random() * int);
+// Функция-генератор возвращает случайное целое число
+// В диапазоне переданных аргументов (включительно)
+// Параметры по умолчанию установлены для получения чисел до 100
+export default (min = 1, max = 99) => {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+};
