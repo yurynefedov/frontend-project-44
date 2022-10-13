120fc4e00e2e3cd0b98a6474adb41fcd87ca762c
yurynefedov <yury.a.nefedov@gmail.com>
2022-10-13 17:51:10 +0400

    refactor: refactoring functions in prime-game.js

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -1,31 +1,31 @@
 import gameEngine from '../index.js';
 import getRandomNumber from '../get-random-number.js';
 
+const isEven = (number) => number % 2 === 0;
+
+const isPrime = (num) => {
+  // Исключаем числа до 1, а также все четные числа, кроме 2, которое является простым
+  // Чтобы условие isEven(num) отработало корректно, первой должна выполняться проверка num === 2
+  // Это позволит нам соркатить количество вычислений
+  // И проверять остаток от деления только на нечетные числа
+  if (num === 2) {
+    return true;
+  }
+  if (num <= 1 || isEven(num)) {
+    return false;
+  }
+  for (let i = 3; i < num; i += 2) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 export default () => {
   const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-  const isEven = (number) => number % 2 === 0;
-
   const generateContent = () => {
-    const isPrime = (num) => {
-    // Исключаем числа до 1, а также все четные числа, кроме 2, которое является простым
-    // Чтобы условие isEven(num) отработало корректно, первой должна выполняться проверка num === 2
-    // Это позволит нам соркатить количество вычислений
-    // И проверять остаток от деления только на нечетные числа
-      if (num === 2) {
-        return true;
-      }
-      if (num <= 1 || isEven(num)) {
-        return false;
-      }
-      for (let i = 3; i < num; i += 2) {
-        if (num % i === 0) {
-          return false;
-        }
-      }
-      return true;
-    };
-
     const question = getRandomNumber();
     const correctAnswer = isPrime(question) ? 'yes' : 'no';
     return [question, correctAnswer];
